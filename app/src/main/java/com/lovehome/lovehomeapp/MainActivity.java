414e7afd5021430f9478cf42eb95ef27a6281a27
zyt028 <1247682546@qq.com>
2016-08-09 15:58:59 +0800

    Initial commit

---
+++ app/src/main/java/com/lovehome/lovehomeapp/MainActivity.java
@@ -0,0 +1,69 @@
+package com.lovehome.lovehomeapp;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.ViewPager;
+import android.util.Log;
+import android.view.View;
+import android.widget.LinearLayout;
+
+import com.lovehome.lovehome.LoveHome;
+import com.lovehome.my.My;
+import com.lovehome.publish.Publish;
+
+import java.util.ArrayList;
+
+public class MainActivity extends FragmentActivity {
+
+    public void myclick(View view){
+        switch(view.getId()){
+            case R.id.home_btn:{
+                FragmentTransaction ft=fm.beginTransaction();
+                LoveHome f=new LoveHome();
+                ft.replace(R.id.content, f);
+                ft.addToBackStack(null);
+                ft.commit();
+                break;
+            }
+            case R.id.publish_btn:{
+                FragmentTransaction ft=fm.beginTransaction();
+                Publish f=new Publish();
+                ft.replace(R.id.content, f);
+                ft.addToBackStack(null);
+                ft.commit();
+                break;
+            }
+            case R.id.wode_btn:{
+                FragmentTransaction ft=fm.beginTransaction();
+                My f=new My();
+                ft.replace(R.id.content, f);
+                ft.addToBackStack(null);
+                ft.commit();
+                break;
+            }
+        }
+    }
+
+
+    FragmentManager fm;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(getWindow().FEATURE_NO_TITLE);
+
+        setContentView(R.layout.activity_main);
+
+        //加载的第一个碎片界面
+        fm=getSupportFragmentManager();
+        FragmentTransaction ft=fm.beginTransaction();
+        LoveHome lh=new LoveHome();
+        ft.add(R.id.content, lh);
+        ft.commit();
+
+    }
+}
---
+++ lovehome/src/main/java/com/lovehome/lovehome/LoveHome.java
@@ -0,0 +1,21 @@
+package com.lovehome.lovehome;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Administrator on 2016/8/9.
+ */
+public class LoveHome extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater,  ViewGroup container,  Bundle savedInstanceState) {
+
+        View v=inflater.inflate(R.layout.lovehome_layout, null);
+        return v;
+
+    }
+
+}
